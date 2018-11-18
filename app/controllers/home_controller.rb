6908fc55e9d71c3d442253cb7139e930c51825d4
Samir Haq <samirhaq031@gmail.com>
2018-11-18 02:44:13 -0500

    Webhooks changes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,24 @@
 class HomeController < ShopifyApp::AuthenticatedController
-  def index
+  
+  def list
     @products = ShopifyAPI::Product.find(:all, params: { limit: 10 })
     @webhooks = ShopifyAPI::Webhook.find(:all)
+
+    unless @webhooks.present?
+      create_order
+    end
+
+  end
+
+  def create_order
+  # create webhook for order creation if it doesn't exist
+    unless ShopifyAPI::Webhook.find(:all).any?
+      webhook = {
+        topic: 'orders/create',
+        address: "https://#https://floating-tundra-99940.herokuapp.com/home/webhook/order_create",
+        format: 'json'}
+
+      ShopifyAPI::Webhook.create(webhook)
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root :to => 'home#index'
   mount ShopifyApp::Engine, at: '/'
+  post 'home/create_order' => 'home#create_order'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
